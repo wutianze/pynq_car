7eaca45c4490feadc7393ab0e986363553373bfb
sauron <1369130123@qq.com>
2019-10-28 18:01:24 +0800

    paper update

--- Pynq-Part/Car/ini.sh
+++ Pynq-Part/Car/ini.sh
@@ -4,10 +4,10 @@
  # @Email: 1369130123qq@gmail.com
  # @Date: 2019-10-14 14:51:04
  # @LastEditors: Sauron Wu
- # @LastEditTime: 2019-10-28 17:19:06
- # @Description: must run in su mode
+ # @LastEditTime: 2019-10-28 17:51:08
+ # @Description: Must run in su mode and after running, the program should be run in su mode, or you will get Gtk Warning: cannot open display.
+
  ###
 insmod ~/dpu.ko
 xauth merge /home/xilinx/.Xauthority
 mkdir src/build
-# Then run collcet in su mode, or you will get Gtk Warning: cannot open display.
