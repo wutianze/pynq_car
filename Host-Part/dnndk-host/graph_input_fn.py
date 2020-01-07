27c7c0b5ffc32a92dd48bfdea58a7a552ce3c840
sauron <1369130123@qq.com>
2020-01-07 12:03:17 +0800

    add remove function in collect and fix some bugs

--- Host-Part/dnndk-host/graph_input_fn.py
+++ Host-Part/dnndk-host/graph_input_fn.py
@@ -1,34 +1,19 @@
 '''
 @Author: sauron
 @Date: 2019-10-29 15:33:55
-@LastEditTime : 2020-01-03 14:36:19
-@LastEditors  : Please set LastEditors
+@LastEditTime : 2020-01-07 11:59:29
+@LastEditors  : Sauron Wu
 @Description: In User Settings Edit
 @FilePath: /pynq_car/Host-Part/dnndk-host/graph_input_fn.py
 '''
 import cv2
-from PIL import Image
+#from PIL import Image
 import os
 import numpy as np
 def image_handle(img):
     return (img[40:,:])/255.0-0.5
 CONV_INPUT = "conv2d_1_input"
 calib_batch_size = 50
-
-#path = "/home/xilinx/dnndk-pynqz2/yolo_keras/prepare_training_data/trainData/images/"
-
-def letterbox_image(image, size):
-        '''resize image with unchanged aspect ratio using padding'''
-        iw, ih = image.size
-        w, h = size
-        scale = min(w/iw, h/ih)
-        nw = int(iw*scale)
-        nh = int(ih*scale)
-
-        image = image.resize((nw,nh), Image.BICUBIC)
-        new_image = Image.new('RGB', size, (128,128,128))
-        new_image.paste(image, ((w-nw)//2, (h-nh)//2))
-        return new_image
 def calib_input(iter):
   images = []
   path = "/home/xilinx/pynq_car/Host-Part/images/"
@@ -38,13 +23,5 @@ def calib_input(iter):
           continue
       image = image_handle(cv2.imread(path + files[iter*calib_batch_size + index]))
       images.append(image)
-      
-      # for yolo
-      #image = Image.open(path+files[index])
-      #boxed_image = letterbox_image(image,(416,416))
-      #image_data = np.array(boxed_image, dtype='float32')
-      ##print(image_data.shape)
-      #image_data /= 256.
-      #images.append(image_data)
   return {CONV_INPUT: images}
 
