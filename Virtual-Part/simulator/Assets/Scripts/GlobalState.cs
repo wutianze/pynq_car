45b86d7a939ba61cf4b707f275c5223eec1fdabb
sauron <1369130123@qq.com>
2020-02-14 09:30:27 +0800

    delete Virtual Part

--- Virtual-Part/simulator/Assets/Scripts/GlobalState.cs
+++
@@ -1,9 +0,0 @@
-﻿
-public static class GlobalState {
-
-    public static bool bAutoHideSceneMenu = false;
-    public static bool bAutoConnectToWebSocket = false;
-    public static string log_path = "default";
-    public static string script_path = "default";
-
-}
